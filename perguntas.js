f7eb0c93852a805a2462f21575f77e64ce5aa024
vitorroziski <138133421+vitorroziski@users.noreply.github.com>
2024-10-22 09:42:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    '1. Normalmente, quantos litros de sangue uma pessoa tem?',
+    'Entre 4 a 6 litros. São retirados 450 mililitros.'
+)
+
+criaCartao(
+    'Historia',
+    '2. De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes.'
+)
+
+criaCartao(
+    'Historia',
+    '3. De onde é a invenção do chuveiro elétrico?',
+    'Brasil.'
+)
+
+criaCartao(
+    'Geografia',
+    '4. Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia.'
+)
